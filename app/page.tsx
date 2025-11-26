473ea8436e6c176460c59c7d139137d6e2ac65d0
Harsh Sawant <harshsawant2505@gmail.com>
2025-11-26 18:54:20 +0530

    address change

--- app/page.tsx
+++ app/page.tsx
@@ -484,7 +484,7 @@ function FeaturedProjectSection() {
   const projects = [
     {
       id: 1,
-      title: "Padukone-Dravid Centre for Sports Excellence",
+      title: "World Centre for Sports Excellence",
       image: "https://images.unsplash.com/photo-1574629810360-7efbbe195018?w=1000&h=600&fit=crop",
       area: "15,000",
       sports: "8",
@@ -978,10 +978,7 @@ export default function AmicoHomepage() {
                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                   </svg>
                   <span>
-                    Second Floor, 657,<br />
-                    100 feet Road,<br />
-                    Binnamangala I Stage,<br />
-                    Indira Nagar, Bengaluru 560038.<br />
+                  Address: 14/122,<br /> Shipra Path, Mansarovar Sector 1,<br /> Mansarovar, Jaipur, Rajasthan 302020 <br />
                     <a href="#" className="underline decoration-1 underline-offset-2 hover:text-gray-200">View on Google Maps</a>
                   </span>
                 </li>
@@ -989,14 +986,14 @@ export default function AmicoHomepage() {
                   <svg className="w-5 h-5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                   </svg>
-                  <span>+91-8951980194</span>
+                  <span>+91-8209730974</span>
                 </li>
                 <li className="flex items-center gap-3">
                   <svg className="w-5 h-5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  <a href="mailto:infra@amicosports.com" className="hover:text-gray-200">infra@amicosports.com</a>
+                  <a href="mailto:info@amicosports.com" className="hover:text-gray-200">info@amicosports.com</a>
                 </li>
               </ul>
             </div>
